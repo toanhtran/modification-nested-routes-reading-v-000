fdd59a5839276d37074d9d2157f7d8a50f423b3f
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-16 21:06:51 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,8 +33,18 @@ class PostsController < ApplicationController
   end
 
   def edit
+  if params[:author_id]
+    author = Author.find_by(id: params[:author_id])
+    if author.nil?
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = author.posts.find_by(id: params[:id])
+      redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+    end
+  else
     @post = Post.find(params[:id])
   end
+end
 
   private
 
